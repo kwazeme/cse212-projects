9024168f8d90d6095c5649b9b0307c1fb23cf94f
kwazeme <97014623+kwazeme@users.noreply.github.com>
2026-01-03 22:34:45 +0200

    ArraySelector: Done

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -11,6 +11,23 @@ public static class ArraySelector
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return [];
+        var result = new int[select.Length];
+        var array1Index = 0;
+        var array2Index = 0;
+
+        for (int i = 0; i < select.Length; i++)
+        {
+            if (select[i] == 1)
+            {
+                result[i] = list1[array1Index];
+                array1Index++;
+            }
+            else if (select[i] == 2)
+            {
+                result[i] = list2[array2Index];
+                array2Index++;
+            }
+        }
+        return result;
     }
 }
