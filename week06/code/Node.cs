e8ce22e0a84bfd5a271a192efdd6e6e21b37bdcc
kwazeme <97014623+kwazeme@users.noreply.github.com>
2026-02-13 21:05:29 +0200

    Week06 - Trees Done

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,6 +12,12 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
+        // update insert to insert unique values only (i.e. if the value is already in the tree, do not insert it again)
+        if (value == Data)
+        {
+            // Value already exists in the tree, do not insert it again
+            return;
+        }
 
         if (value < Data)
         {
@@ -34,12 +40,50 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        // search for a value; 
+        // return true if found 
+        // otherwise return false
+        if (value == Data)
+        {
+            return true;
+        }
+        else if (value < Data)
+        {
+            // Search to the left
+            if (Left is null)
+                return false;
+            else
+                return Left.Contains(value);
+        }
+        else
+        {
+            // Search to the right
+            if (Right is null)
+                return false;
+            else
+                return Right.Contains(value);
+        }
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        // find the height of the longest path from this node to a leaf node
+        
+        int leftHeight = 0;
+        int rightHeight = 0;
+
+        if (Left != null)
+        {
+            leftHeight = Left.GetHeight();
+        }
+
+        if (Right != null)
+        {
+            rightHeight = Right.GetHeight();
+        }
+    // return the lenght of the longest path from this node to a leaf node plus one
+    
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
